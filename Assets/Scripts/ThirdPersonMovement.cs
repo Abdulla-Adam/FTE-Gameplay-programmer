8708e48c4f3e6ded07eeeb854fc8ba5643fcb17b
Abdulla-Adam <muhammadbd53@gmail.com>
2022-09-07 21:39:39 +0500

    Blend tree and jump animation
    Basic movement animation using blend tree. Implemented jump animation with a bug.

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,240 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    //Set up Animator
+    private Animator anim;
+    private bool IsMoving;
+    private bool IsPunch;
+    private bool IsSprint;
+    private bool IsJumping;
+    public bool IsGrounded;
+    
+    //Set Up Speed
+     private float moveSpeed = 2.5f;
+    [SerializeField] private float WalkSpeed = 2.5f;
+    [SerializeField] private float SprintSpeed = 8f;
+    [SerializeField] private float velocity = 0f;
+    [SerializeField] private float acceleration = 0.1f;
+    [SerializeField] private float deceleration = 0.1f;
+    [SerializeField] private float JumpTimeout = 1f;
+
+    private int velocityHash;
+
+    //Set Character Controller
+    public CharacterController characterController;
+    //Set Rotation Smoothness
+    private float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+    //Set Cam
+    public Transform cam;
+
+    public float GroundedOffset = 0;
+    public float GroundedRadius = 1.7f;    
+    
+    public LayerMask GroundLayers;
+
+
+ //   void SetAnimationState(string state, bool condition)
+ //   {
+ //       anim.SetBool
+ //   }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Getting Player Object Animator Component
+        anim = GetComponent<Animator>();
+        velocityHash = Animator.StringToHash("Velocity");
+        characterController.detectCollisions = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+        GroundedCheck();
+    }
+
+
+    void HandleInput()
+    {
+                //Getting Keyboard Input
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+        //Storing Keyboard Input into a Direction Vector
+        Vector3 Direction = new Vector3(Horizontal, 0f, Vertical).normalized;
+
+        if(Input.GetButton("Fire1"))
+        {
+            //Set Animation transition
+            if (IsPunch != true)
+            {
+                anim.SetBool("IsPunch", true);
+                IsPunch = true;
+            }
+        }
+
+        else
+        {
+            //Set Animation transition
+            if (IsPunch == true)
+            {
+                anim.SetBool("IsPunch", false);
+                IsPunch = false;
+            }
+        }
+
+        //Start Movement if Direction Vector has magnitude
+        if (Direction.magnitude >= 0.1f )
+        {
+            anim.SetBool("IsPunch", false);
+            IsPunch = false;
+            //Calculate Movement direction angle and set Smooth
+            float TargetAngle = Mathf.Atan2(Direction.x, Direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, TargetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            //Set Rotation according to Angle 
+            transform.rotation = Quaternion.Euler(0f, smoothAngle, 0);
+
+            //Set Movement in accordance with Camera angle
+            Vector3 moveDir = Quaternion.Euler(0f, TargetAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
+            //Set Animation transition
+            if (IsMoving != true)
+            {
+                anim.SetBool("IsMoving", true);
+                IsMoving = true;
+                
+            }
+
+            //Set Animation transition
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if (IsSprint != true)
+                {
+                    anim.SetBool("IsSprint", true);
+                    IsSprint = true;
+                    moveSpeed = SprintSpeed;          
+                }
+            }
+
+            else
+            {
+                //Set Animation transition
+                if (IsSprint == true)
+                {
+                    anim.SetBool("IsSprint", false);
+                    IsSprint = false;
+                    moveSpeed = WalkSpeed;
+                    
+                }
+            }
+
+        }
+
+        
+
+        else if (IsMoving == true)
+        {
+            anim.SetBool("IsMoving", false);
+            IsMoving = false;
+           
+        }
+
+        MoveAnimation();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {           
+            JumpTimeout = 1f;
+            IsGrounded = false;
+            anim.SetBool("IsJumping" , true);
+            IsJumping = true;
+            
+        }
+
+        
+
+    }
+
+    void MoveAnimation()
+    {
+        bool movementButtonPressed = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
+        
+        if (movementButtonPressed && velocity < 1f )
+        {
+            if (IsMoving && velocity < 0.5f)
+            {
+                // velocity = Time.deltaTime * acceleration;    
+                velocity = Mathf.Lerp(velocity , 0.5f , acceleration);
+            }
+            
+            if (IsSprint)
+            {
+                // velocity += Time.deltaTime * acceleration;    
+                velocity = Mathf.Lerp(velocity , 1f , acceleration);
+            }
+            
+            if (!IsSprint && velocity >= 0.5f)
+            {
+                velocity -= Time.deltaTime * deceleration;                
+            }
+
+        }
+
+        if (!movementButtonPressed && velocity > 0f )
+        {            
+            velocity -= Time.deltaTime * deceleration;                           
+        }
+        
+        if (!movementButtonPressed && velocity < 0f )
+        {
+            velocity = 0f;
+        }
+
+        anim.SetFloat(velocityHash , velocity);
+    }
+
+ private void GroundedCheck()
+        {
+            // set sphere position, with offset
+            Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset,
+                transform.position.z);
+            IsGrounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers,
+                QueryTriggerInteraction.Ignore);
+            
+
+
+    
+            if (JumpTimeout <= 0)
+            {
+               
+               anim.SetBool("IsJumping" , false);
+               
+               if (IsJumping)
+               {
+                 transform.position = new Vector3(transform.position.x , transform.position.y + 0.1f , transform.position.z); 
+                 IsJumping = false;
+               }
+               
+            }   
+
+
+            if (IsJumping)
+            {
+                if (JumpTimeout > 0)
+                {
+                    JumpTimeout -= Time.deltaTime;    
+                }              
+
+                if (JumpTimeout <= 0)
+                {
+                    JumpTimeout = 0;
+                    transform.position = new Vector3(transform.position.x , transform.position.y - 0.1f , transform.position.z); 
+                }              
+            }
+        }
+
+}
+
